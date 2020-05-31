40b058d037d96f0d2a165f3fa772166687aa6053
okumaryu <okumaryu@gmail.com>
2020-05-31 16:54:29 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
-lock 3.14.0
+lock '3.14.0'
 
 # 自身のアプリ名、リポジトリ名を記述
-set :application, FURIMA_72a
-set :repo_url,  git@github.com:okumaryu/FURIMA_72a.git'
+set :application, 'FURIMA_72a'
+set :repo_url,  'git@github.com:okumaryu/FURIMA_72a.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
