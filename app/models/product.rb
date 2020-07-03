20128c36eed3fb397e17b1a8a04de1c7fbabae72
wataru-a-72 <now.or.never34@icloud.com>
2020-07-03 16:10:48 +0900

    モデル作成（Active hash 以外）

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+  belongs_to :brand
+  belongs_to :category
+  belongs_to_active_hash :size
+  belongs_to_active_hash :product_condition
+  belongs_to_active_hash :postage_payer
+  belongs_to_active_hash :shipping_date
+  has_many :productphotoes, dependent: :destroy
+  #has_many :likes, dependent: :destroy
+  #has_many :comments, dependent: :destroy
+  #belongs_to :seller, class_name: "User"
+  #belongs_to :buyer, class_name: "User"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_03_064800) do
+ActiveRecord::Schema.define(version: 2020_07_03_070956) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "productphotos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
