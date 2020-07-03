d41a27324795e544bd9b2ad060f87bfe8af6cd53
doiyasanryo <s10.i8.h88@outlook.jp>
2020-07-03 10:54:01 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  #root 'items#create'
-  root 'products#show'
+  # root 'items#create'
+  # root 'products#show'
+  root 'mypage#index'
   resources :items, only: [:new, :create] do
   end
 end
