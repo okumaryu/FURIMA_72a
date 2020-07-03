8fa906cd0d677d9bf525e6b701489a25e2471083
wataru-a-72 <now.or.never34@icloud.com>
2020-07-03 17:08:06 +0900

    モデル編集（Activehash以外）

--- db/migrate/20200703063827_create_products.rb
+++ db/migrate/20200703063827_create_products.rb
@@ -1,9 +1,19 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.string :name
-      t.integer :price
-      t.timestamps
+      t.string :name, null: false
+      t.integer :price, null: false
+      t.text :description, null: false
+      t.integer	:brand_id, foreign_key: true
+      t.string :category_id, null: false, foreign_key: true
+      t.integer	:size_id,null: false, foreign_key: true
+      t.integer	:product_condition_id	null: false, foreign_key: true
+      t.integer	:prefecture_code, null: false
+      t.integer	:postage_payer_id, null: false, foreign_key: true
+      t.integer	:shipping_date_id, null:false, foreign_key: true
+      t.integer	:seller_id, null: false, foreign_key: true
+      t.integer	:buyer_id, null: false, foreign_key: true
+      #t.timestamps
     end
   end
 end
--- db/migrate/20200703064800_create_productphotos.rb
+++ db/migrate/20200703064800_create_productphotos.rb
@@ -1,9 +1,8 @@
 class CreateProductphotos < ActiveRecord::Migration[5.2]
   def change
     create_table :productphotos do |t|
-      t.string :src
-      t.references :product, foreign_key: true
-      t.timestamps
+      t.string :url,null: false
+      t.integer	:product_id, null: false, foreign_key: true
     end
   end
 end
