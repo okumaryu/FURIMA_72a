858127047443f1c4db501addab386cd5dc796c44
doiyasanryo <s10.i8.h88@outlook.jp>
2020-07-07 13:35:29 +0900

    ログアウトページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'mypage#index'
   resources :items, only: [:new, :create] do
   end
+  resources :mypage, only: [:index] do
+  end
+  get '/logout', to: 'mypage#logout'
 end
