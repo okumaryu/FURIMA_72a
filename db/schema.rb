128e7089dd9b2c0d8705ea8a08a4132103e477c1
okumaryu <okumaryu@gmail.com>
2020-07-13 21:42:18 +0900

    commit
    DBへのカラム追加
    会員情報登録のhamlファイル作成

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,16 @@
 ActiveRecord::Schema.define(version: 2020_06_18_135331) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "family_name", null: false
+    t.string "first_name", null: false
+    t.string "family_name_rubi", null: false
+    t.string "first_name_rubi", null: false
+    t.integer "birth_year", null: false
+    t.integer "birth_month", null: false
+    t.integer "birth_day", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
