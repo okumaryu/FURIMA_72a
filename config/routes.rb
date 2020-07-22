8af7b811b96d930476864b80415cc306dbd13ea3
okumaryu <okumaryu@gmail.com>
2020-07-22 21:14:14 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  root 'mypage#index' 
+  devise_for :users, :controllers => {
+ :registrations => 'users/registrations',
+ :sessions => 'users/sessions',
+ :passwords     => "users/passwords"
+}
+ devise_scope :user do
+  get 'users/signuplist' => 'users/registrations#signuplist'
+  # ↓な気もしますのでrails routesコマンドなどで確認してください
+  # get 'signuplist' => 'users/registrations#signuplist'
+end
+    # resources :users, only: [:new,:edit,:update]
 
-  devise_for :users 
-  root 'mypage#index'
-  resources :items, only: [:new, :create] do
-  end
-    resources :users, only: [:new,:edit,:update]
+  # root 'mypage#index' 
+  resources :items, only: [:new, :create]
 end
