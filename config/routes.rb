be99209ce6a3fffb177ca594f348b81cb99e659d
wataru-a-72 <now.or.never34@icloud.com>
2020-07-24 20:50:00 +0900

    JS実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#show'
-  resources :products, only: [:new,:create,:show]
+  resources :products, only: [:new,:create,:show] do
+    #Ajaxで動くアクションのルートを作成
+    collection do
+      get 'category/get_category_children', to: 'products#get_category_children', defaults: { format: 'json' }
+      get 'category/get_category_grandchildren', to: 'products#get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
   resources :mypage, only: [:index] do
     collection do
       get :logout
