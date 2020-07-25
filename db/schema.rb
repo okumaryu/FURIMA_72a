77c79db682184dc05d138cbdf451dd3a708a60ab
okumaryu <okumaryu@gmail.com>
2020-07-25 18:46:31 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_135331) do
+ActiveRecord::Schema.define(version: 2020_07_13_033356) do
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_brands_on_product_id"
+  end
+
+  create_table "productphotos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "src"
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_productphotos_on_product_id"
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "description", null: false
+    t.integer "brand_id"
+    t.integer "size_id", null: false
+    t.integer "productcondition_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "postagepayer_id", null: false
+    t.integer "shippingdate_id", null: false
+    t.integer "seller_id"
+    t.integer "buyer_id"
+    t.integer "category_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "family_name", null: false
-    t.string "first_name", null: false
-    t.string "family_name_rubi", null: false
-    t.string "first_name_rubi", null: false
-    t.integer "birth_year", null: false
-    t.integer "birth_month", null: false
-    t.integer "birth_day", null: false
-    t.text "introduction"
-    t.string "avatar"
-    t.string "destination_family_name", null: false
-    t.string "destination_first_name", null: false
-    t.string "destination_family_name_rubi", null: false
-    t.string "destination_first_name_rubi", null: false
-    t.integer "zipcode", null: false
-    t.integer "prefecture_code", default: 0, null: false
-    t.string "city", null: false
-    t.string "house_number", null: false
-    t.string "building_name", null: false
-    t.integer "phone_number", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -44,4 +53,6 @@ ActiveRecord::Schema.define(version: 2020_06_18_135331) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "brands", "products"
+  add_foreign_key "productphotos", "products"
 end
