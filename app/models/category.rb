a871829da7045e763c9f114b729dc60309f5959b
doiyasanryo <s10.i8.h88@outlook.jp>
2020-08-01 13:40:47 +0900

    商品詳細表示

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveHash::Base
   self.data = [
-    {id: 0, category: '選択してください'}, {id: 1, category: 'レディース'},{id: 2, category: 'ベビー・キッズ'},{id: 3, category: 'インテリア・住まい・小物'},
-    {id: 4, category: '本・音楽・ゲーム'},{id: 5, category: 'おもちゃ・ホビー・グッズ'},{id: 6, category: 'コスメ・香水・美容'},{id: 7, category: '家電・スマホ・カメラ'},
-    {id: 8, category: 'スポーツ・レジャー'},{id: 9, category: 'ハンドメイド'},{id: 10, category: 'チケット'},{id: 11, category: '自転車・オートバイ'},{id: 12, category: 'その他'}
+    {id: 0, name: '選択してください'}, {id: 1, name: 'レディース'},{id: 2, name: 'ベビー・キッズ'},{id: 3, name: 'インテリア・住まい・小物'},
+    {id: 4, name: '本・音楽・ゲーム'},{id: 5, name: 'おもちゃ・ホビー・グッズ'},{id: 6, name: 'コスメ・香水・美容'},{id: 7, name: '家電・スマホ・カメラ'},
+    {id: 8, name: 'スポーツ・レジャー'},{id: 9, name: 'ハンドメイド'},{id: 10, name: 'チケット'},{id: 11, name: '自転車・オートバイ'},{id: 12, name: 'その他'}
   ]
 end
