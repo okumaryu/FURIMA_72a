7027081d03864f579c9d522c0d435cfde6e263b7
wataru-a-72 <now.or.never34@icloud.com>
2020-08-09 19:19:53 +0900

    css編集

--- db/migrate/20200703063827_create_products.rb
+++ db/migrate/20200703063827_create_products.rb
@@ -6,7 +6,6 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.text :description, null: false
       t.string :brand_id, foreign_key: true
       t.string :category_id, null: false, foreign_key: true
-      t.integer	:size_id,null: true, foreign_key: true
       t.integer	:productcondition_id,	null: false, foreign_key: true
       t.integer	:prefecture_id, null: false
       t.integer	:postagepayer_id, null: false, foreign_key: true
