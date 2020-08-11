269092fecffc2fd30f243bdf949a7ec6fc42e787
wataru-a-72 <now.or.never34@icloud.com>
2020-08-11 22:43:01 +0900

    JS修正

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -32,8 +32,9 @@ $(document).on('turbolinks:load', ()=> {
   let input = document.querySelector('#product_price');
   const log = document.getElementById('handlefee');
   const profit = document.getElementById('profit');
+  
   input.addEventListener('input', updateValue);
-    function updateValue(e) {
+    function updateValue() {
       let val1 = parseInt(input.value, 10);
       let output = val1 * 0.1
       let gain = val1 - output
