bace6e3fe5ca734b71a35ce494cdfab1b2ce82d5
okumaryu <okumaryu@gmail.com>
2020-08-13 17:29:54 +0900

    commit

--- db/migrate/20200703063827_create_products.rb
+++ db/migrate/20200703063827_create_products.rb
@@ -5,7 +5,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.integer :price, null: false
       t.text :description, null: false
       t.string :brand_id, foreign_key: true
-      t.string :category_id, null: false, foreign_key: true
+      t.integer :category_id, null: false, foreign_key: true
       t.integer	:productcondition_id,	null: false, foreign_key: true
       t.integer	:prefecture_id, null: false
       t.integer	:postagepayer_id, null: false, foreign_key: true
