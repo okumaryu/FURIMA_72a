8c7b4de3c2c1d4908ac965bd10fe5287c932c6ae
okumaryu <okumaryu@gmail.com>
2020-08-13 17:21:03 +0900

    commit

--- db/migrate/20200703063827_create_products.rb
+++ db/migrate/20200703063827_create_products.rb
@@ -5,7 +5,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.integer :price, null: false
       t.text :description, null: false
       t.string :brand_id, foreign_key: true
-      t.string :category_id, null: false, foreign_key: true
+      t.integer :category_id, null: false, foreign_key: true
       t.integer	:size_id,null: true, foreign_key: true
       t.integer	:productcondition_id,	null: true, foreign_key: true
       t.integer	:prefecture_id, null: false
@@ -13,7 +13,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.integer	:shippingdate_id, null:false, foreign_key: true
       t.integer	:seller_id, null: true #foreign_key: true
       t.integer	:buyer_id#, null: false, foreign_key: true
-      t.integer :productphoto_id, null: false, foreign_key: true
+      # t.integer :productphoto_id, null: false, foreign_key: true
     end
   end
 end
---
+++ db/seeds/product.rb
@@ -0,0 +1,19 @@
+Products.create(
+  [
+    {
+      id: 1,
+      name: "メンズ服01",  
+      price: 8000,
+      description:"" ,
+      condition: 0,
+      category_id: 5
+      user_id: 1,
+    },
+    {
+
+    },
+    {
+
+    },
+  ]
+)
