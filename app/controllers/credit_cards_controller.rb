0a9e98451b6e0fd8368b975cf0a810416cd54297
sotani824 <sotani824@gmail.com>
2020-08-14 19:45:54 +0900

    Update credit_cards_controller.rb

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,2 +1,26 @@
 class CreditCardsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+
+    if params["payjp_token"].blank?
+      redirect_to action: "new", alert: "クレジットカードを登録できませんでした。"
+    else
+      customer = Payjp::Customer.create(
+        email: current_user.email,
+        card: params["payjp_token"],
+        metadata: {user_id: current_user.id}
+      )
+      @card = CreditCard.new(user_id: current_user.id, token: payjp_token)
+      if @card.save
+        redirect_to user_path(current_user)
+      else
+        render :new
+      end
+    end
+  end
+  
 end
