dd6e211b2035872630c893f70a835f39d7dade5f
okumaryu <okumaryu@gmail.com>
2020-08-15 10:05:49 +0900

    最大表示数を5→4に変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all.order('id DESC').limit(5)
+    @products = Product.all.order('id DESC').limit(4)
   end
 
   def show
