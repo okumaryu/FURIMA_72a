b877349e67a41ad973c15b3bd24b6ae95c9b5331
sotani824 <sotani824@gmail.com>
2020-08-15 18:54:19 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_14_225812) do
+ActiveRecord::Schema.define(version: 2020_07_24_094215) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
